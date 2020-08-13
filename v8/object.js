7e345d416a4c352bd27e13442e94c3bfd9b00508
gsio <gsio@protonmail.com>
2020-08-13 20:01:12 +0700

    complete js

--- v8/object.js
+++ v8/object.js
@@ -1,29 +1,59 @@
-let ClassCache  = {};
+let cache = [];
 
 class Object {
     static class_name = 'gc::Object';
 
+    static reg() {
+        _registerClass(this, this.class_name);   
+    }
+
     static new() {
-        let Cls = ClassCache[this.class_name];
-        if (!Cls) {
-            Cls = this;
-            registerClass(Cls, this.class_name);
-            ClassCache[this.class_name] = Cls;
-        }
 
         let argv = [];
         for (let i = 0, t = arguments.length; i < t; ++i) {
             argv.push(arguments[i]);
         }
-
-        let obj = new (Function.prototype.bind.apply(Cls, argv));
-        newObject(obj, Cls, argv);
-        obj.initialize();
+        let obj = new (Function.prototype.bind.apply(this, [null].concat(argv)));
+        _newObject(obj, this.class_name, argv);
 
         return obj;
     }
 
-    initialize() {}
+    destory() {
+        _destroyObject(this);
+    }
+
+    _call(name) {
+        let argv = [];
+        for (let i = 1, t = arguments.length; i < t; ++i) {
+            argv.push(arguments[i]);
+        }
+        return _call.apply(this, [name, argv]);
+    }
+
+    static _call(name) {
+        let argv = [];
+        for (let i = 1, t = arguments.length; i < t; ++i) {
+            argv.push(arguments[i]);
+        }
+        return _callStatic.apply(this, [name, argv]);
+    }
+
+    _keep() {
+        let idx = cache.indexOf(this);
+        if (idx < 0) {
+            cache.push(this);
+        }
+    }
+
+    _release() {
+        let idx = cache.indexOf(this);
+        if (idx >= 0) {
+            cache.splice(idx, 1);
+        }
+    }
 }
+
+Object.reg();
 
 module.exports = Object;
